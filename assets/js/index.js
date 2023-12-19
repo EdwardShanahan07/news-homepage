28cc5fc03b6c4232b1763b154ba9f300e4c2deb2
Edward Shanahan <edwardshanahan357@gmail.com>
2023-12-19 19:09:05 +0000

    Add response navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,12 @@
-document.getElementById("menu-icon").addEventListener("click", () => {
+function toggleMenu() {
   const menuIcon = document.getElementById("menu-icon");
+  const menu = document.getElementById("menu");
+  const overlay = document.querySelector(".overlay");
+
   menuIcon.classList.toggle("active");
-});
+  menu.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+document.querySelector(".overlay").addEventListener("click", toggleMenu);
+document.getElementById("menu-icon").addEventListener("click", toggleMenu);
